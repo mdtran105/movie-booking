165a7358ed103b27efdce9af6990aadda04c4511
DucTran <mdtran105@gmail.com>
2023-12-12 18:01:01 +0700

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,22 @@ export default function Cart() {
   return (
     <div className="col-4 cart">
       <h1>DANH SÁCH GHẾ BẠN CHỌN</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim harum placeat, iure reiciendis perferendis culpa. At ex facere magnam ad.</p>
+      <table className='tableCart table table-bordered border-white align-middle'>
+        <thead>
+          <tr>
+            <th className='text-white'>Số ghế</th>
+            <th className='text-white'>Giá tiền</th>
+            <th className='text-white'>Hủy</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>A2</td>
+            <td>30.000</td>
+            <td><button className='btn text-danger'>X</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
